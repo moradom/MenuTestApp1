c8ee7775a66a5dcd3ab0ea458c7d96a454570d6a
Marco Morado <marco.morado@preito.com>
2017-10-11 13:03:27 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "MenuTestApp1",
+  "homePage" : "Main",
+  "name" : "MenuTestApp1",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
